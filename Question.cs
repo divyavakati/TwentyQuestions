24595d6a89bce35a335e46c8fbafdc043eda0a9d
DivyaVakati <vkdivya1@gmail.com>
2016-07-09 15:37:34 -0700

    Program.cs First

---
+++ Question.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyQuestions
+{
+    class Question
+    {
+        public string question;
+        public Question yes;
+        public Question No;
+
+    }
+}
